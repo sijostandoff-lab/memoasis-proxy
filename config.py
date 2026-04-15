eaf545e79e74be8f2822bd34539a6c3e5d637deb
sijostandoff-lab <sijo.standoff@gmail.com>
2026-04-15 22:10:00 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+PORT = 4443
+# 32 символа. Это базовый ключ.
+USERS = {
+    "memoasis": "7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d"
+}
+# Маскируемся под гугл, чтобы DPI нас не зарезал
+TLS_DOMAIN = "google.com"
