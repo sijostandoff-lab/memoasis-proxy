3aad25b2a0a62000a1cb398e79a4f7f6feb3b35a
sijostandoff-lab <sijo.standoff@gmail.com>
2026-04-15 22:46:35 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-PORT = 7777
-# 32 символа. Это базовый ключ.
+PORT = 9000
 USERS = {
     "memoasis": "7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d"
 }
-# Маскируемся под гугл, чтобы DPI нас не зарезал
 TLS_DOMAIN = "google.com"
